d41670f9b202cf61fe722a38dc2572b44ebb5af8
learngiteasy <cuemath.teacher.21@gmail.com>
2021-11-12 02:43:34 +0530

    first commit for spring project

---
+++ src/main/java/_springgit/_springgit_1/App.java
@@ -0,0 +1,13 @@
+package _springgit._springgit_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
